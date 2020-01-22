1ed5ad2545370f7a7d7791869295608214f310e8
Dave Whiteland <davewhiteland@users.noreply.github.com>
2020-01-22 00:33:19 +0000

    add a tapir to the webpage

--- src/main/java/Http.java
+++ src/main/java/Http.java
@@ -20,7 +20,15 @@ public class Http {
   static class RootHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange req) throws IOException {
-      String message = "<h1>Hello CS1813!</h1>";
+      String message = "<!DOCTYPE html>\n"
+        + "<html><head><title>Hello world</title></head>"
+        + "<body style='text-align:center'><h1>Hello CS1813!</h1>"
+        + "<a href='https://en.wikipedia.org/wiki/Mountain_tapir'><img "
+        + "src='https://upload.wikimedia.org/wikipedia/commons/thumb/"
+        + "2/21/Tapirus_pinchaque_portrait.jpg/"
+        + "800px-Tapirus_pinchaque_portrait.jpg' alt='tapir'/></a>"
+        + "<p>Image CC BY 2.0 <a href='https://www.flickr.com/photos/dsifry/'"
+        + ">David Sifry</a></p></body></html>";
 
       Headers headers = req.getResponseHeaders();
       headers.add("Content-Type", "text/html; charset=UTF-8");
